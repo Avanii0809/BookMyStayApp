24151cb07166bfa55fb5c3b9a1380f08400de21e
Avanii0809 <as080906@gmail.com>
2026-03-16 15:44:32 +0530

    UC3

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,7 +1,47 @@
 public class BookMyStayApp {
-    public static void main (String [] args){
-        System.out.println("Welcome to the Hotel Booking Management System");
-        System.out.println("System initialized successfully.");
+    Room[] rooms = new Room[5];
+    int count = 0;
+
+    public void addRoom(Room room) {
+        if (count < rooms.length) {
+            rooms[count] = room;
+            count++;
+        } else {
+            System.out.println("Inventory full!");
+        }
     }
 
+    public void displayRooms() {
+        System.out.println("Hotel Room Inventory Data");
+        System.out.println("-------------------------");
+
+        for (int i = 0; i < count; i++) {
+            System.out.println("Room No: " + rooms[i].roomNumber);
+            System.out.println("Type: " + rooms[i].type);
+            System.out.println("Price per night: " + rooms[i].pricePerNight);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        BookMyStayApp inventory = new BookMyStayApp();
+        Room r1 = new Room(101, "Single", 1000.0);
+        Room r2 = new Room(102, "Double", 1500.0);
+        Room r3 = new Room(103, "Suite", 3000.0);
+        inventory.addRoom(r1);
+        inventory.addRoom(r2);
+        inventory.addRoom(r3);
+        inventory.displayRooms();
+    }
+}
+
+class Room {
+    int roomNumber;
+    String type;
+    double pricePerNight;
+    public Room(int roomNumber, String type, double pricePerNight) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.pricePerNight = pricePerNight;
+    }
 }
